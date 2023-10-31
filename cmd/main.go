5b902087a058d7cc7ba87ab482795f817aecefa4
whyy <icarovsilva1@gmail.com>
2023-10-30 21:38:53 -0300

    feat: add gql in project

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,32 @@
-package main 
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/IcaroSilvaFK/gql-go/graph"
+	"github.com/go-chi/chi"
+)
+
+const defaultPort = "8080"
 
 func main() {
-	println("Hey little stranger")
+	port := os.Getenv("PORT")
+
+	mx := chi.NewRouter()
+
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	mx.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	mx.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, mx))
 }
